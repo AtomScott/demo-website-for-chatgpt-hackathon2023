ad4e168eb207dafaf856364fe7c08a51bd8e6dbc
Atom Scott <atom.james.scott@gmail.com>
2023-05-02 07:38:39 +0900

    use jekyll

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 AOS.init();
-const swiper = new Swiper(".swiper-container", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-  },
-  loop: true,
-});
 const countdownElement = document.getElementById("countdown");
-// The reason for setting the time to 16:30:00Z is that Tokyo is 9 hours ahead of Coordinated Universal Time (UTC), 
-// so when it is 9:30 AM in Tokyo, it is 0:30 AM UTC. 
-// Therefore, you need to subtract 9 hours from 9:30 AM to convert it to UTC time, which is 16:30:00 on the previous day, May 5th.
-const deadline = new Date("2023-04-30T15:00:00Z"); // Set your deadline date here
+const deadline = new Date("2023-04-30T15:00:00Z");
 
 function updateCountdown() {
   const now = new Date();
@@ -39,3 +26,62 @@ function updateCountdown() {
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const mentorsSection = document.getElementById("mentors");
+  const modals = document.querySelectorAll('.modal');
+  
+  mentorsSection.addEventListener("click", (event) => {
+    const button = event.target.closest(".openModal");
+    if (!button) return;
+
+    const targetModalId = button.getAttribute('data-modal-id');
+    const targetModal = document.getElementById(targetModalId);
+    targetModal.classList.remove('opacity-0');
+    targetModal.classList.remove('pointer-events-none');
+  });
+
+  modals.forEach(modal => {
+    const closeModalButton = modal.querySelector('.closeModal');
+    
+    closeModalButton.addEventListener('click', () => {
+      modal.classList.add('opacity-0');
+      modal.classList.add('pointer-events-none');
+    });
+    
+    modal.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        modal.classList.add('opacity-0');
+        modal.classList.add('pointer-events-none');
+      }
+    });
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.getElementById('nav-toggle');
+  const navContent = document.getElementById('nav-content');
+  let menuOpen = false;
+
+  navToggle.addEventListener('click', () => {
+    navContent.classList.toggle('hidden');
+    menuOpen = !menuOpen;
+
+    // Change the menu icon to a cross when the menu is open
+    if (menuOpen) {
+      navToggle.innerHTML = `
+        <svg class="fill-current h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <title>Close</title>
+          <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+        </svg>`;
+    } else {
+      navToggle.innerHTML = `
+        <svg class="fill-current h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+        </svg>`;
+    }
+  });
+});
